7487412c5334419d4f1c66eea44ff6baa1f1a22d
Cristian Encinas Gayo <panicoyalice@gmail.com>
2017-01-10 20:34:18 +0100

    ejercicio 1 del examen
    Ejercicio 1 del examen corregido en clase

---
+++ Ejercicio1_Examen_Cristian/Assets/scripts/marcador.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+						//Se coloca en las porterías
+public class marcador : MonoBehaviour {
+	public Text texto_marcador;
+	int goles = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (goles >= 1) {
+			SceneManager.LoadScene ("nave");
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D col){
+		if(col.transform.name == "balon"){
+			goles += 1;
+				texto_marcador.text = goles + "";
+		}
+	}
+}
---
+++ Ejercicio1_Examen_Cristian/Assets/scripts/movimientopj1.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimientopj1 : MonoBehaviour {
+    Rigidbody2D rb;
+    // Use this for initialization
+    void Start() {
+		rb = GetComponent<Rigidbody2D> ();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.UpArrow)){
+			rb.velocity = new Vector2 (0,10);
+        }
+		if (Input.GetKeyDown(KeyCode.DownArrow)){
+			rb.velocity = new Vector2 (0,-10);
+		}
+		if (Input.GetKeyDown(KeyCode.LeftArrow)){
+			rb.velocity = new Vector2 (-10,0);
+		}
+		if (Input.GetKeyDown(KeyCode.RightArrow)){
+			rb.velocity = new Vector2 (10,0);
+		}
+//------------------------------------GETKEYUP------------------------------------------
+		if (Input.GetKeyUp(KeyCode.UpArrow)){
+			rb.velocity = new Vector2 (0,0);
+		}
+		if (Input.GetKeyUp(KeyCode.DownArrow)){
+			rb.velocity = new Vector2 (0,0);
+		}
+		if (Input.GetKeyUp(KeyCode.LeftArrow)){
+			rb.velocity = new Vector2 (0,0);
+		}
+		if (Input.GetKeyUp(KeyCode.RightArrow)){
+			rb.velocity = new Vector2 (0,0);
+		}
+    }
+}
