87b76d27026ffb172124f7f591569cdd4297fe43
Cristian Encinas Gayo <panicoyalice@gmail.com>
2017-01-11 18:42:44 +0100

    ejercicio 2 casi corregido

---
+++ Ejercicio1_Examen_Cristian/Assets/scripts/nave/explota.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class explota : MonoBehaviour {
+	public GameObject plataforma;
+	public GameObject explosion;
+	public Animator panel_animator; 
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D col){
+		panel_animator.SetBool ("visible", true); //"visible" es el nombre del bool (true o false) de la animacion
+		Instantiate (explosion, transform.position,transform.rotation); //todo lo que está en minúsculas es sobre uno mismo. Instantiate llama a la explosion y hay que darle 2 vectores
+		Destroy (gameObject);
+		Destroy (plataforma);
+	}
+}
---
+++ Ejercicio1_Examen_Cristian/Assets/scripts/nave/nave.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class nave : MonoBehaviour { //solo se puede dar impuslo a objetos con rigidbody
+	public float fuerza = 10f;
+	public float rotation = 0.5f;
+	public ParticleSystem propulsor;
+	Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.Space)) {
+			rb.AddForce (transform.up * fuerza);
+			propulsor.Play ();
+		}
+		if (Input.GetKey (KeyCode.LeftArrow)){
+
+	}
+}
